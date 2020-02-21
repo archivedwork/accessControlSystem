88c8a7e4b95541ccbd511445db4691a79158610a
Mohammed Jamal Hadi Alhayaj <tbruej@INF.ELTE.HU>
2020-02-21 11:54:35 +0100

    add requirements

---
+++ accessControlSystem.py
@@ -0,0 +1,8 @@
+### Task 1 ##
+## - choose one of three problems
+## - code the selected problem and make three version as below:
+## - version 1 : use structured code base (try to apply some design pattern style)
+## - version 2 : make half structured and half unstructured code
+## - version 3 : make unstructured code base (code that is hard to read and follow)
+
+
