5cf5fb13fa82c2e71fe92a13c3218b37e6bd0181
MO <mohammed.alhayaj@ibm.com>
2020-02-24 10:32:19 +0100

    main file

--- accessControlSystem.py
+++ accessControlSystem.py
@@ -5,4 +5,19 @@
 ## - version 2 : make half structured and half unstructured code
 ## - version 3 : make unstructured code base (code that is hard to read and follow)
 
+# version 1
+import driver
+import data
 
+a = []
+a.append(driver.createAP(data.ACCESS_POINT_1))
+
+
+if a[0] == data.ACCESS_POINT_1:
+    print "DONE"
+
+a.append(driver.createAP(data.ACCESS_POINT_1))
+if a[1] == data.ACCESS_POINT_1:
+    print "FAIL"
+
+print(a)
