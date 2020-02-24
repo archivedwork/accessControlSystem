24972c2f828bd2a132ab19f22e7978fb49038c8d
MO <mohammed.alhayaj@ibm.com>
2020-02-24 10:32:42 +0100

    driver file that contains functions

---
+++ driver.py
@@ -0,0 +1,10 @@
+def createAP(accessIdentifier):
+    return accessIdentifier
+
+
+def createUser(userIdentifier):
+    return userIdentifier
+
+def grant(accessIdentifier, userIdentifier):
+    return 0
+
