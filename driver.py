20e8f58c40f1a13495b5b3928f1814fd5fbb4f39
MO <mohammed.alhayaj@ibm.com>
2020-02-24 13:11:30 +0100

    some functions modifications

--- driver.py
+++ driver.py
@@ -5,6 +5,14 @@ def createAP(accessIdentifier):
 def createUser(userIdentifier):
     return userIdentifier
 
+def deleteAP(accessIdentifier):
+    return accessIdentifier
+
 def grant(accessIdentifier, userIdentifier):
+    return True
+
+def check(accessIdentifier, userIdentifier):
     return 0
 
+def revoke(accessIdentifier, userIdentifier):
+    return 0
